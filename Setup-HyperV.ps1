b80aa4a45fa8aab08d73388e011800e3ebef7b6c
Dennis McDonald <dennis@cambridgecomputing.us>
2022-10-31 11:31:26 -0500

    Added needed options on configuration lines.

--- Setup-HyperV.ps1
+++ Setup-HyperV.ps1
@@ -15,7 +15,7 @@
 # Install and setup defaults
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 Set-VMHost -VirtualMachinePath "D:\" -VirtualHardDiskPath "D:\" -MaximumVirtualMachineMigrations 4 -MaximumStorageMigrations 4 -VirtualMachineMigrationAuthenticationType CredSSP -VirtualMachineMigrationPerformanceOption SMB
-Set-VMReplicationServer $true -AllowedAuthenticationType Kerberos -ReplicationAllowedFromAnyServer $true
+Set-VMReplicationServer $true -AllowedAuthenticationType Kerberos -DefaultStorageLocation D:\ -ReplicationAllowedFromAnyServer $true
 
 # Setup Networking. Requires a network adapter "Trunk" in its name.
-New-VMSwitch "Trunk" -EnableIov -NetAdapterName (Get-NetAdapter | Where-Object {$_.Name -like "*trunk*"}).Name -AllowManagementOS $false
+New-VMSwitch "Trunk" -EnableIov $true -NetAdapterName (Get-NetAdapter | Where-Object {$_.Name -like "*trunk*"}).Name -AllowManagementOS $true
